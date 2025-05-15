3ed6f929881e927c0c55426aacc4110e29ea6582
toxinu <toxinu@gmail.com>
2025-05-15 11:29:12 +0900

    fix: clean

---
+++ rename.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# New author details
+NEW_NAME="toxinu"
+NEW_EMAIL="jo@kikitama.com"
+
+# Use git filter-branch to update all commits
+git filter-branch --env-filter "
+if [ -n \"\$GIT_COMMITTER_NAME\" ]; then
+    export GIT_COMMITTER_NAME=\"$NEW_NAME\"
+    export GIT_COMMITTER_EMAIL=\"$NEW_EMAIL\"
+fi
+if [ -n \"\$GIT_AUTHOR_NAME\" ]; then
+    export GIT_AUTHOR_NAME=\"$NEW_NAME\"
+    export GIT_AUTHOR_EMAIL=\"$NEW_EMAIL\"
+fi
+" --tag-name-filter cat -- --branches --tags
+
+# Optional: Clean up backup refs created by filter-branch
+rm -rf .git/refs/original/
--- src/content/fixed/games.md
+++
@@ -1,9 +0,0 @@
-// Not used
-+++
-title = "Games"
-+++
-Below is a short list of games I have worked on mostly during game jams.
-
-* **[Pod2000](https://grena.itch.io/pod2000)** - In Pod2000, you play an agent belonging to a huge corporation that exploits planets. To exploit a planet, you need to install modules to extract oil, refinery, etc.
-
-* **[Fórnir](https://ldjam.com/events/ludum-dare/43/fornir)** - In Fórnir you play a Jarl who rules over a village. Try your best to keep the trust of your people by making good decisions…
